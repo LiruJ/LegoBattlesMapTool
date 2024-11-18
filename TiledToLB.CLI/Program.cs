8148e9bb00a7c39204fce513d203c4dcbd54fc83
Laura <laura@lovebirb.com>
2024-11-18 11:06:19 +0100

    Removed old command parsing system

--- TiledToLB.CLI/Program.cs
+++ TiledToLB.CLI/Program.cs
@@ -2,7 +2,6 @@
 using TiledToLB.CLI.CommandLine;
 using TiledToLB.Core;
 
-
 var parseResult = Parser.Default.ParseArguments<ProcessOptions, ProcessLBZOptions, UnpackOptions>(args);
 
 await parseResult.WithParsedAsync<ProcessOptions>(async options => await Processor.ProcessMapAsync(options.InputFilePath, options.OutputDirectoryPath));
@@ -10,38 +9,3 @@ await parseResult.WithParsedAsync<ProcessLBZOptions>(async options => await Proc
 await parseResult.WithParsedAsync<UnpackOptions>(async options => await Processor.UnpackRomAsync(options.InputFilePath, options.OutputDirectoryPath, options.Silent));
 
 await parseResult.WithNotParsedAsync((errors) => Console.Out.WriteLineAsync("Error parsing inputs!"));
-
-
-
-//await Processor.UnpackRomAsync(p.InputFilePath, p.OutputDirectoryPath, p.Silent);
-
-//CommandLineOptions? options = null;
-//Parser.Default.ParseArguments<CommandLineOptions>(args).WithParsed((parsedOptions) =>
-//{
-//    // Set the options.
-//    if (parsedOptions.Validate())
-//        options = parsedOptions;
-//}).WithNotParsed((errors) => Console.WriteLine("Error parsing inputs!"));
-
-//if (options == null || !options.Silent)
-//    await Console.Out.WriteLineAsync("Created by Liru. Credit goes to CUE for the decompressors for LZX.");
-
-//if (options == null)
-//    return;
-
-//switch (options.ExecutionMode)
-//{
-//    case ExecutionMode.ProcessMap:
-//        await Processor.ProcessMapAsync(options.InputFile!, options.OutputFile!);
-//        break;
-//    case ExecutionMode.ProcessMapLBZ:
-//        await Processor.ProcessAndPackLBZAsync(options.InputFile!, options.OutputFile!);
-//        break;
-//    case ExecutionMode.UnpackRom:
-//        await Processor.UnpackRomAsync(options.RomFile!, options.TiledTemplateOutput!, options.Silent);
-//        break;
-//    case ExecutionMode.Invalid:
-//        throw new Exception("Invalid command line parameters!");
-//    default:
-//        break;
-//}
